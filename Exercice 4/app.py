27714426fc953187c49e4b8ee6c52bcc8fd5338a
nullhq <taboutsafredy@gmail.com>
2025-10-21 08:27:53 +0100

    exo4: correction de l'app pour convertir proprement une image reçu en matrice de pixel

--- Exercice 4/app.py
+++ Exercice 4/app.py
@@ -1,31 +1,30 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
 from tensorflow import keras
+from PIL import Image
 import numpy as np
 
 app = Flask(__name__)
 
-# Chargement du mod le Keras
+# Chargement du modèle Keras
 model = keras.models.load_model('mnist_model.h5')
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    # V rification des donn es
-    if 'image' not in data:
-        return jsonify({'error': 'No image provided'}), 400
+    if 'image' not in request.files:
+        return jsonify({'error': "Aucun fichier n'a été fournis !"}), 400
 
-    image_data = np.array(data['image'])
-    # Assurez-vous que l'image est au bon format (1, 784) et normalis e
-    image_data = image_data.reshape(1, 784)
-    image_data = image_data.astype("float32") / 255.0
+    file = request.files['image']
+    image = Image.open(file.stream).convert('L')  # Convert to grayscale
+    image = image.resize((28, 28))
+    image_data = np.array(image).reshape(1, 784).astype("float32") / 255.0
 
     prediction = model.predict(image_data)
-    predicted_class = np.argmax(prediction, axis=1)[0]
+    predicted_class = int(np.argmax(prediction, axis=1)[0])
 
     return jsonify({
-        'prediction': int(predicted_class),
+        'prediction': predicted_class,
         'probabilities': prediction.tolist()
     })
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
